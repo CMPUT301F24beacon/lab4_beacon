713ee16bc7bf9ffffe4a2c3574739a5e8e7f1d36
Shaaf Babar <shaaf.babar@gmail.com>
2024-09-27 14:40:01 -0600

    shape File

--- app/src/main/java/com/example/beacon/Shape.java
+++ app/src/main/java/com/example/beacon/Shape.java
@@ -3,7 +3,7 @@ package com.example.beacon;
 public abstract class Shape {
     private int x;
     private int y;
-    String color = "Indigo";
+    String color = "$PUT_YOUR_CHOICE_OF_COLOR";
 
     public Shape(int x, int y)
     {
