aeee45461dd26c2b27c58a93e1b8fa85526887eb
samia225 <155067354+samia225@users.noreply.github.com>
2024-09-27 13:44:41 -0600

    Updated Shape.java by adding the color field

--- app/src/main/java/com/example/beacon/Shape.java
+++ app/src/main/java/com/example/beacon/Shape.java
@@ -3,6 +3,7 @@ package com.example.beacon;
 public abstract class Shape {
     private int x;
     private int y;
+    String color = "$PUT_YOUR_CHOICE_OF_COLOR";
 
     public Shape(int x, int y)
     {
