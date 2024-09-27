94b303682b0f88abd975e7d4bf6ee0042987bfed
Shaaf Babar <shaaf.babar@gmail.com>
2024-09-27 13:28:01 -0600

    circle extends shape

--- app/src/main/java/com/example/beacon/Circle.java
+++ app/src/main/java/com/example/beacon/Circle.java
@@ -1,20 +1,32 @@
 package com.example.beacon;
 
-public abstract class Shape {
-
+public class Circle extends Shape {
     private double radius;
-
+    private final double PI = 3.14;
 
     public Circle(int x, int y, double radius) {
-        super(x,y);
+        super(x, y);
         this.radius = radius;
     }
 
-    public double getRadius(){
-        return radius
+    public double getRadius() {
+        return radius;
     }
 
     public void setRadius(double radius) {
         this.radius = radius;
     }
-    
+
+    public double getArea() {
+        return PI * radius * radius;
+    }
+
+    public double getCircumference() {
+        return 2 * PI * radius;
+    }
+
+    @Override
+    public String toString() {
+        return "circle of radius: " + radius + " at position (" + x + ", " + y + ")";
+    }
+}
